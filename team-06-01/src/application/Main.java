18088ca0cba31a6df3afa2b4e638bcaaa33a0c91
yaoty <yaotyrae@gmail.com>
2021-04-24 15:38:34 -0700

    - Added more comments

--- team-06-01/src/application/Main.java
+++ team-06-01/src/application/Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 
 /* Originally, we were going to use PDFBox to read PDFs but we are unsure if we allowed to use PDF.
  * We are now only using txt files until we are sure that we can use other libraries.
- * PDFBox is a library to open up pdfs. */
+ * PDFBox is a library to open up pdfs. 
+ * This program takes in a TXT file and translates it to the users wanted language
+ * Although with non English letters the translation prints out what seems to be random characters. These characters cannot be understood because we cannot print languages
+ * such as Chinese, Japanese, Hindi etc.
+ * */
 
 public class Main{
 	
